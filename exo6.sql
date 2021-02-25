a7527bcd0933693f0d70c49f32455e3bcce2f89d
florentpriouret <78485326+florentpriouret@users.noreply.github.com>
2021-02-25 10:12:59 +0100

    Update exo6.sql

--- exo6.sql
+++ exo6.sql
@@ -11,22 +11,22 @@ INSERT INTO employees_2 (id,first_name,last_name,salary) VALUES
 (3,'Antoine','DUPUY',2200),
 (4,'Jérémy','DURANT',1650),
 (5,'Julie','DUMONT',3800),
-(6,'Léo','DUBOIS',3560)
+(6,'Léo','DUBOIS',3560);
 /*2) Mettre à jour toutes les lignes de la table employees_2 en générant (grâce à une fonction) des valeurs pour la
 colonne "userid".
 La fonction utilisée doit permettre de générer une valeur en prenant les 3 premières lettres du prénom et les 5
 premières lettres du nom de famille de chaque employé.*/
 UPDATE employees_2 
 SET userid = (
-  select concat(left(first_name,3),left(last_name,5)))
+  select concat(left(first_name,3),left(last_name,5)));
 /*3) Changer le nom de famille de l'employé "DURANT" en "DUROI"*/
 UPDATE employees_2 SET last_name='DUROI'
-WHERE last_name ='DURANT'
+WHERE last_name ='DURANT';
 /*4) Augmenter de 10% le salaire de tous les employés ayant un "o" dans leur prénom*/
 UPDATE employees_2 
 SET salary =salary*1.1 
-WHERE first_name LIKE '%o%'
+WHERE first_name LIKE '%o%';
 /*5) Supprimer tous les employés dont le salaire est inférieur à 2000*/
 DELETE 
 FROM employees_2 
-WHERE salary<2000
+WHERE salary<2000;
